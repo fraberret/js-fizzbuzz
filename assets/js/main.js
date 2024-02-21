d05a0cb2c8cf2ad85ddc53485c143d960e4dd339
fraberret <f.berretta02@gmail.com>
2024-02-21 15:19:20 +0100

    fix code and add if i is multiple of 3 and five then fizzbuzz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,27 @@
 
 //creare ciclo
 for (let i=1; i<101; i++){
-    console.log(i);
+    //console.log(i);
     //se multiplo di 3 Fizz 
-    if(i%3==0){
-        console.log("Fizz");
-    }
-    //se multiplo di 5 Buzz
-    else if (i%5==0){
-        console.log("Buzz");
+    if(i%3===0 && i%5===0){
+        console.log(`${i} FizzBuzz`);//stampa i risultati
     }
     //se multiplo di 3 e 5 Fizz buzz
+    else if (i%3===0){
+        console.log(`${i} Fizz`);//stampa i risultati
+    }
+    //se multiplo di 5 Buzz
+    else if (i%5===0){
+        console.log(`${i} Buzz`);//stampa i risultati
+    }
+    //se non multiplo
+    else {
+        
+        console.log(i);//stampa i risultati
+    }
+    
 
 
    
     
 }
-//stampa i risultati
