7d6b22b1339e2058958241122bddd596684648af
fraberret <f.berretta02@gmail.com>
2024-02-21 15:09:31 +0100

    add buzz if i is multiple of 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,13 @@ for (let i=1; i<101; i++){
         console.log("Fizz");
     }
     //se multiplo di 5 Buzz
+    else if (i%5==0){
+        console.log("Buzz");
+    }
+    //se multiplo di 3 e 5 Fizz buzz
+
+
    
     
 }
-//se multiplo di 3 e 5 Fizz buzz
 //stampa i risultati
