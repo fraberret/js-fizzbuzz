b3ab6a3e08082f1111077f474fd4cd145afa64d4
fraberret <f.berretta02@gmail.com>
2024-02-21 15:03:24 +0100

    add if i multiple of 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 
 //creare ciclo
-for (let i=0; i<101; i++){
+for (let i=1; i<101; i++){
     console.log(i);
+    //se multiplo di 3 Fizz 
+    if(i%3==0){
+        console.log("Fizz");
+    }
+    //se multiplo di 5 Buzz
+   
+    
 }
-//se multiplo di 3 Fizz 
-//se multiplo di 5 Buzz
 //se multiplo di 3 e 5 Fizz buzz
 //stampa i risultati
