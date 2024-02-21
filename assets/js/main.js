08ce81cb0ec9baca64debfa7bda5f0dce1d0bafe
fraberret <f.berretta02@gmail.com>
2024-02-21 16:54:32 +0100

    add list on page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,31 @@
+const ul = document.getElementById("list")
 
 //creare ciclo
-for (let i=1; i<101; i++){
-    //console.log(i);
-    //se multiplo di 3 Fizz 
-    if(i%3===0 && i%5===0){
-        console.log(`${i} FizzBuzz`);//stampa i risultati
-    }
+for (let i = 1; i < 101; i++) {
+
+
     //se multiplo di 3 e 5 Fizz buzz
-    else if (i%3===0){
-        console.log(`${i} Fizz`);//stampa i risultati
+    if (i % 3 === 0 && i % 5 === 0) {
+        //console.log(`${i} FizzBuzz`);//stampa risultati
+        markupItem = `<li>FizzBuzz</li>`
+        ul.innerHTML += markupItem
+
+    }
+    //se multiplo di 3 Fizz 
+    else if (i % 3 === 0) {
+        //console.log(`${i} Fizz`);//stampa risultati
+        markupItem = `<li>Fizz</li>`
+        ul.innerHTML += markupItem
     }
     //se multiplo di 5 Buzz
-    else if (i%5===0){
-        console.log(`${i} Buzz`);//stampa i risultati
+    else if (i % 5 === 0) {
+        //console.log(`${i} Buzz`);//stampa risultati
+        markupItem = `<li>Buzz</li>`
+        ul.innerHTML += markupItem
     }
-    //se non multiplo
     else {
-        
-        console.log(i);//stampa i risultati
+
+        let markupItem = `<li>${i}</li>`
+        ul.innerHTML += markupItem
     }
-    
-
-
-   
-    
 }
